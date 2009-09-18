a234600f95d2ae602d5eb680efee7908af2d2839
Raphaël Londeix <hotgloupi@trollteam.com>
2009-09-18 09:59:34 +0000

    Ajout des sélecteur CSS2, doc des globals

--- pyjs/core/dom.js
+++ pyjs/core/dom.js
@@ -374,6 +374,134 @@ function regEscape($string) {
 // -----------------------------------------------------------------------
 // modules
 // -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+// selectors
+// -----------------------------------------------------------------------
+
+// child selector
+selectors[">"] = function($results, $from, $tagName, $namespace) {
+	var $element, i, j;
+	for (i = 0; i < $from.length; i++) {
+		var $subset = childElements($from[i]);
+		for (j = 0; ($element = $subset[j]); j++)
+			if (compareTagName($element, $tagName, $namespace))
+				$results.push($element);
+	}
+};
+
+// sibling selector
+selectors["+"] = function($results, $from, $tagName, $namespace) {
+	for (var i = 0; i < $from.length; i++) {
+		var $element = nextElementSibling($from[i]);
+		if ($element && compareTagName($element, $tagName, $namespace))
+			$results.push($element);
+	}
+};
+
+// attribute selector
+selectors["@"] = function($results, $from, $attributeSelectorID) {
+	var $test = attributeSelectors[$attributeSelectorID].test;
+	var $element, i;
+	for (i = 0; ($element = $from[i]); i++)
+		if ($test($element)) $results.push($element);
+};
+
+// -----------------------------------------------------------------------
+// pseudo-classes
+// -----------------------------------------------------------------------
+
+pseudoClasses["first-child"] = function($element) {
+	return !previousElementSibling($element);
+};
+
+pseudoClasses["lang"] = function($element, $code) {
+	$code = new RegExp("^" + $code, "i");
+	while ($element && !$element.getAttribute("lang")) $element = $element.parentNode;
+	return $element && $code.test($element.getAttribute("lang"));
+};
+
+// -----------------------------------------------------------------------
+//  attribute selectors
+// -----------------------------------------------------------------------
+
+// constants
+AttributeSelector.NS_IE = /\\:/g;
+AttributeSelector.PREFIX = "@";
+// properties
+AttributeSelector.tests = {};
+// methods
+AttributeSelector.replace = function($match, $attribute, $namespace, $compare, $value) {
+	var $key = this.PREFIX + $match;
+	if (!attributeSelectors[$key]) {
+		$attribute = this.create($attribute, $compare || "", $value || "");
+		// store the selector
+		attributeSelectors[$key] = $attribute;
+		attributeSelectors.push($attribute);
+	}
+	return attributeSelectors[$key].id;
+};
+AttributeSelector.parse = function($selector) {
+	$selector = $selector.replace(this.NS_IE, "|");
+	var $match;
+	while ($match = $selector.match(this.match)) {
+		var $replace = this.replace($match[0], $match[1], $match[2], $match[3], $match[4]);
+		$selector = $selector.replace(this.match, $replace);
+	}
+	return $selector;
+};
+AttributeSelector.create = function($propertyName, $test, $value) {
+	var $attributeSelector = {};
+	$attributeSelector.id = this.PREFIX + attributeSelectors.length;
+	$attributeSelector.name = $propertyName;
+	$test = this.tests[$test];
+	$test = $test ? $test(this.getAttribute($propertyName), getText($value)) : false;
+	$attributeSelector.test = new Function("e", "return " + $test);
+	return $attributeSelector;
+};
+AttributeSelector.getAttribute = function($name) {
+	switch ($name.toLowerCase()) {
+		case "id":
+			return "e.id";
+		case "class":
+			return "e.className";
+		case "for":
+			return "e.htmlFor";
+		case "href":
+			// http://www.glennjones.net/Post/809/getAttributehrefbug.htm
+			return "e.getAttribute('href',2)";
+	}
+	return "e.getAttribute('" + $name.replace($NAMESPACE, ":") + "')";
+};
+
+// -----------------------------------------------------------------------
+//  attribute selector tests
+// -----------------------------------------------------------------------
+
+AttributeSelector.tests[""] = function($attribute) {
+	return $attribute;
+};
+
+AttributeSelector.tests["="] = function($attribute, $value) {
+	return $attribute + "==" + Quote.add($value);
+};
+
+AttributeSelector.tests["~="] = function($attribute, $value) {
+	return "/(^| )" + regEscape($value) + "( |$)/.test(" + $attribute + ")";
+};
+
+AttributeSelector.tests["|="] = function($attribute, $value) {
+	return "/^" + regEscape($value) + "(-|$)/.test(" + $attribute + ")";
+};
+
+// -----------------------------------------------------------------------
+//  parsing
+// -----------------------------------------------------------------------
+
+// override parseSelector to parse out attribute selectors
+var _parseSelector = parseSelector;
+parseSelector = function($selector) {
+	return _parseSelector(AttributeSelector.parse($selector));
+};
 
 // -------- >>      insert modules here for packaging       << -------- \\
 
--- pyjs/core/globals.js
+++ pyjs/core/globals.js
@@ -1,6 +1,39 @@
+/**
+ * fileOverview Globals functions, generaly shortcuts
+ * @author <a href="mailto:raphael.londeix@gmail.com"> Raphaël Londeix </a>
+ * @version 0.1
+ */
+/** scope _global_ */
 
+/**
+ * Get element by id
+ * @param {String} id The element id
+ * @returns {Element|null} First element found, if exists or null
+ */
 $ = function $(id) {return document.getElementById(id);};
+
+/**
+ * Query elements with CSS2 syntax
+ * @param {String} selectors CSS2 selectors
+ * @param {String|String[]|Element|Element[]} [ref] reference(s) node(s) to search in
+ * @returns {Element[]} Array of results
+ * @see py.dom.query
+ */
 $$ = function $$(selectors, ref) {return py.dom.query(selectors, ref);};
+
+/**
+ * Create an Element
+ * @param {String} tag Tag name
+ * @param {Object} [obj] attributes of the node
+ * @returns {Element} created element
+ * @see py.dom.create
+ */
 $c = function $c(tag){return document.createElement(tag);};
-$t = function $t(){return document.createTextNode(arguments);};
+
+/**
+ * Create a text node
+ * @param {String} str Text
+ * @returns {Element} text node
+ */
+$t = function $t(str){return document.createTextNode(str);};
 
