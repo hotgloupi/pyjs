84f8a1fb9d75843afafa66e89356ea8f874a498e
Raphaël Londeix <raphael.londeix@gmail.com>
2011-07-09 20:55:13 +0200

    unobfuscate onload hack

--- pyjs/core/core.js
+++ pyjs/core/core.js
@@ -44,13 +44,36 @@ var py = null;
 (function(){
     var dom_loaded = false;
     // http://www.kryogenix.org/days/2007/09/26/shortloaded
-    (function(i) {var u =navigator.userAgent;var e=/*@cc_on!@*/false; var st =
-    setTimeout;if(/webkit/i.test(u)){st(function(){var dr=document.readyState;
-    if(dr=="loaded"||dr=="complete"){i();}else{st(arguments.callee,10);}},10);}
-    else if((/mozilla/i.test(u)&&!/(compati)/.test(u)) || (/opera/i.test(u))){
-    document.addEventListener("DOMContentLoaded",i,false); } else if(e){     (
-    function(){var t=document.createElement('doc:rdy');try{t.doScroll('left');
-    i();t=null;}catch(e){st(arguments.callee,0);}})();}else{window.onload=i;}})(function() {
+    (function (i) {
+        var e = /*@cc_on!@*/false;
+        var u = navigator.userAgent;
+        if (/webkit/i.test(u)) {
+            setTimeout(function() {
+                var dr = document.readyState;
+                if (dr == "loaded" || dr == "complete") {
+                    i();
+                } else {
+                    setTimeout(arguments.callee, 10);
+                }
+            }, 10);
+        }
+        else if ((/mozilla/i.test(u) && !/(compati)/.test(u)) || (/opera/i.test(u))) {
+            document.addEventListener("DOMContentLoaded", i, false);
+        } else if (e) {
+            (function() {
+                var t = document.createElement('doc:rdy');
+                try {
+                    t.doScroll('left');
+                    i();
+                    t = null;
+                } catch (e) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        } else {
+            window.onload = i;
+        }
+    })(function() {
         dom_loaded = true;
         if (typeof(py) !== "undefined" && py._onLoad) {
             py._onLoad();
