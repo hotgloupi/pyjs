e02d211ea7796c59546351b126a821098f9908c8
Raphaël Londeix <raphael.londeix@gmail.com>
2011-07-09 20:40:17 +0200

    jslint globals

--- pyjs/core/core.js
+++ pyjs/core/core.js
@@ -3,7 +3,11 @@
  * @author <a href="mailto:raphael.londeix@gmail.com">Raphaël Londeix</a>
  * @version 0.1
  */
-
+*global clearInterval: false, clearTimeout: false, document: false, event: false,
+        frames: false, history: false, Image: false, location: false, name: false,
+        navigator: false, Option: false, parent: false, screen: false,
+        setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false
+*/
 if (typeof console != "undefined") {
     if (typeof console.log.apply != "undefined") {
         if (typeof log === "undefined") {
